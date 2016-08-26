8c2a94944410512ec3712ac59a1bc1a71cc69c0e
roberthtux <roberthtux@gmail.com>
2016-08-26 18:45:50 -0500

    Modify DatabaseSeeder
    Se agregan seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TurnsTableSeeder::class);
         $this->call(UserTypesSeeder::class);
+        $this->call(TurnsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(ClientsTableSeeder::class);
         $this->call(CardsTableSeeder::class);
         $this->call(CardTransactionsTableSeeder::class);
         $this->call(ItemsTableSeeder::class);
         $this->call(AccountingAccountsTableSeeder::class);
+        $this->call(ProfilesTableSeeder::class);
+        $this->call(OptionsTableSeeder::class);
+        $this->call(ProfileOptionTableSeeder::class);
+        $this->call(UserProfileTableSeeder::class);
     }
 }
