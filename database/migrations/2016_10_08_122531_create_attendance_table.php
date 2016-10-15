05a0e986d1e2f0227196758afd2d3b85a893afb0
roberthtux <roberthtux@gmail.com>
2016-10-15 13:05:21 -0500

    Add migration
    attendance

---
+++ database/migrations/2016_10_08_122531_create_attendance_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance', function (Blueprint $table){
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->time('checkin');
+            $table->time('checkout');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('attendance');
+    }
+}
